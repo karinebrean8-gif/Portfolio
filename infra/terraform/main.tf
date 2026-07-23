a365f14f42a95d0ab416e8c7505e28b601d5c5ee
Shakib Mia <karinebrean8@gmail.com>
2026-07-23 19:55:00 +0600

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -16,7 +16,6 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   region = var.region
 
@@ -28,18 +27,15 @@ provider "aws" {
     }
   }
 }
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
-
   name = "${var.project}-vpc"
   cidr = var.vpc_cidr
 
   azs             = var.availability_zones
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
-
   enable_nat_gateway   = true
   single_nat_gateway   = false
   enable_dns_hostnames = true
@@ -49,20 +45,16 @@ module "vpc" {
     Component = "network"
   }
 }
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
   cluster_name    = "${var.project}-eks"
   cluster_version = var.kubernetes_version
-
   subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
-
   enable_irsa                    = true
   cluster_endpoint_public_access = false
-
   eks_managed_node_groups = {
     system = {
       desired_size = 3
@@ -80,37 +72,30 @@ module "eks" {
       desired_size = 5
       min_size     = 5
       max_size     = 20
-
       instance_types = ["m7i.large"]
-
       labels = {
         role = "application"
       }
     }
   }
-
   tags = {
     Component = "compute"
   }
 }
-
 resource "aws_ecr_repository" "app" {
   name = "${var.project}/application"
 
   image_scanning_configuration {
     scan_on_push = true
   }
-
   encryption_configuration {
     encryption_type = "AES256"
   }
 }
-
 resource "aws_db_subnet_group" "main" {
   name       = "${var.project}-db-subnet"
   subnet_ids = module.vpc.private_subnets
 }
-
 resource "aws_cloudwatch_log_group" "eks" {
   name              = "/aws/eks/${var.project}"
   retention_in_days = 30
